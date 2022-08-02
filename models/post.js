169cc17133bc0aa64afffb1c2acba8a8ad8c22f7
Auggie <miaugus@gmail.com>
2022-08-02 15:54:20 -0400

    tables created

---
+++ models/post.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const { truncate } = require("./user");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post: {
+      type: DataTypes.STRING,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "User",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Post",
+  }
+);
+
+module.exports = Post;
