b04d49ac5612421250a6bd26c65e13866c6e9ec7
Auggie <miaugus@gmail.com>
2022-08-02 16:59:18 -0400

    user data displays to the page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const { User } = require("../models");
 
-router.get("/", (req, res) => {
-  res.json("I come from the homeRoutes file!");
+router.get("/:id", async (req, res) => {
+  try {
+    const userInfo = await User.findByPk(req.params.id);
+    // convert sequelize object into a usable object
+    const serializedUserData = userInfo.get({ plain: true });
+    res.render("home", serializedUserData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
