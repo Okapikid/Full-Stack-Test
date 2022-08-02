60a8ea9e7dc30019b93f7d5ef3fe9305569c548c
Auggie <miaugus@gmail.com>
2022-08-02 16:38:08 -0400

    get route works

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 // Create new user with /api/user
-
+// Signup
 router.post("/", async (req, res) => {
   try {
     const dbUser = await User.create({
@@ -10,9 +10,23 @@ router.post("/", async (req, res) => {
       password: req.body.password,
       email: req.body.email,
     });
+
     res.status(200).json(dbUser);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+// this is for demonstration only (to see if data can be retrieved)
+// api/user/userInfo:id
+router.get("/userInfo/:id", async (req, res) => {
+  try {
+    const userInfo = await User.findByPk(req.params.id);
+
+    res.status(200).json(userInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
